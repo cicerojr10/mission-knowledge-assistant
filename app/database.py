792ac119aad5fe9d9ed5af76d361123c1d8ba82a
Cicero Quintino Junior <cicerojr10@gmail.com>
2026-06-17 04:26:14 -0300

    feat: add sqlmodel database setup

---
+++ app/database.py
@@ -0,0 +1,19 @@
+from collections.abc import Generator
+
+from sqlmodel import Session, create_engine
+
+
+DATABASE_URL = (
+    "postgresql://mission_user:mission_password"
+    "@localhost:5432/mission_knowledge"
+)
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=True,
+)
+
+
+def get_session() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session
